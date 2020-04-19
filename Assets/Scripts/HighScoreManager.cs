2deb1a4ffe833b97d44ec5202dc4cd261454d7d0
Jasper Stephenson <jasperstephenson@gmail.com>
2020-04-19 20:20:37 +0900

    Added new typeface, fixed bugs in high scores

--- Assets/Scripts/HighScoreManager.cs
+++ Assets/Scripts/HighScoreManager.cs
@@ -27,7 +27,7 @@ public class HighScoreManager : MonoBehaviour
 		playerNameTextObject.GetComponent<TextMeshProUGUI>().text = "";
 		setPlayerScore(time);
 		inputZone.SetActive(false);
-		StartCoroutine(getHighScores());
+		StartCoroutine(getHighScores(true));
 	}
 
 	public void saveScore()
@@ -35,6 +35,10 @@ public class HighScoreManager : MonoBehaviour
 		string name = playerNameTextObject.GetComponent<TextMeshProUGUI>().text;
 		if (name.Length < 2) return;
 		if (name.Length > 12) name = name.Substring(0, 12);
+		name = name.Replace(',', ' ');
+		name = name.Replace('?', '.');
+		name = name.Replace('/', '-');
+		name = name.Replace('\n', ' ');
 		Debug.Log("Adding high score " + playerScore + " for player " + name);
 		StartCoroutine(addHighScore(name, playerScore));
 		inputZone.SetActive(false);
@@ -43,12 +47,12 @@ public class HighScoreManager : MonoBehaviour
 	public void setPlayerScore(float score)
 	{
 		playerScore = score;
-		playerScoreTextObject.GetComponent<TextMeshProUGUI>().text = "Your Time: " + score.ToString("0.000") + 's';
+		playerScoreTextObject.GetComponent<TextMeshProUGUI>().text = "YOUR TIME: " + score.ToString("0.000") + 's';
 	}
 
-	void updateHighScoreLabel()
+	void updateHighScoreLabel(bool shouldIncludePlayer)
 	{
-		bool hasShownPlayer = false;
+		bool hasShownPlayer = !shouldIncludePlayer;
 		string scoreLabel = "";
 		string playerName = playerNameTextObject.GetComponent<TextMeshProUGUI>().text;
 		if (playerName.Length < 2)
@@ -81,7 +85,7 @@ public class HighScoreManager : MonoBehaviour
 		highScoreTextObject.GetComponent<TextMeshProUGUI>().text = scoreLabel;
 	}
 
-	IEnumerator getHighScores()
+	IEnumerator getHighScores(bool shouldIncludePlayer)
 	{
 		Debug.Log("Updating high scores");
 		string url = "https://agile-citadel-44322.herokuapp.com/" + leaderboardName + "/bottom/" + scoreCount.ToString() + '/';
@@ -96,7 +100,7 @@ public class HighScoreManager : MonoBehaviour
 			else if (webRequest.downloadHandler.text == "Forbidden" || webRequest.downloadHandler.text == "Internal Server Error")
 			{
 				Debug.Log("Network Error: " + webRequest.downloadHandler.text);
-				updateHighScoreLabel();
+				updateHighScoreLabel(true);
 				inputZone.SetActive(true);
 			}
 			else
@@ -119,8 +123,11 @@ public class HighScoreManager : MonoBehaviour
 				if (highScoreStrings.Length < scoreCountToDisplay)
 				{
 					scoreCountToDisplay = highScoreStrings.Length;
-					Debug.Log("High Score By Default!");
-					inputZone.SetActive(true);
+					if (shouldIncludePlayer)
+					{
+						Debug.Log("High Score By Default!");
+						inputZone.SetActive(true);
+					}
 				}
 
 				names = new string[scoreCountToDisplay];
@@ -131,20 +138,19 @@ public class HighScoreManager : MonoBehaviour
 					string[] split = highScoreStrings[s].Split(',');
 					names[s] = split[0];
 					scores[s] = split[1];
-					if (playerScore <= float.Parse(split[1]))
+					if (shouldIncludePlayer && playerScore <= float.Parse(split[1]))
 					{
 						Debug.Log("High Score!");
 						inputZone.SetActive(true);
 					}
 				}
 			}
-			updateHighScoreLabel();
+			updateHighScoreLabel(shouldIncludePlayer);
 		}
 	}
 
 	IEnumerator addHighScore(string name, float score)
 	{
-		updateHighScoreLabel();
 		string url = "https://agile-citadel-44322.herokuapp.com/" + leaderboardName + "/add/" + name + '/' + score.ToString("0.00") + '/';
 		using (UnityWebRequest webRequest = UnityWebRequest.Get(url))
 		{
@@ -161,7 +167,7 @@ public class HighScoreManager : MonoBehaviour
 			else
 			{
 				Debug.Log("Successfully added new score");
-				StartCoroutine(getHighScores());
+				StartCoroutine(getHighScores(false));
 			}
 		}
 	}
