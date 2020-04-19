cc2cd3bb5195e97404fe9acfb10ba3570bd0ace9
Jasper Stephenson <jasperstephenson@gmail.com>
2020-04-19 14:54:17 +0900

    Added high scores to level clear screens

--- Assets/Scripts/LevelClearManager.cs
+++ Assets/Scripts/LevelClearManager.cs
@@ -1,49 +1,77 @@
 ﻿using UnityEngine.SceneManagement;
 using UnityEngine;
 
-public class LevelClearManager : MonoBehaviour {
+public class LevelClearManager : MonoBehaviour
+{
 
-    public static LevelClearManager instance;
+	public static LevelClearManager instance;
 
-    public CanvasGroup mainCanvasGroup;
+	public CanvasGroup mainCanvasGroup;
 
-    private const string  LEVEL_CLEAR_SCREEN_PATH = "LevelClearScreen";
-    private static Coroutine levelClearFadeRoutine = null;
-    public static void LevelClear(float timeInSeconds) {
-        Debug.Log(timeInSeconds);
-        if (instance == null) {
-            GameObject gameOverScreenObject = (GameObject)Resources.Load(LEVEL_CLEAR_SCREEN_PATH);
-            GameObject instantiated = Instantiate(gameOverScreenObject);
-            DontDestroyOnLoad(instantiated);
-            instance = instantiated.GetComponent<LevelClearManager>();
-        }
-        instance.ShowLevelClearScreen();
-    }
+	public string nextSceneName;
 
-    private void ShowLevelClearScreen() {
-        Time.timeScale = 0;
-        this.EnsureCoroutineStopped(ref levelClearFadeRoutine);
-        this.CreateAnimationRoutine(
-            0.3f,
-            delegate (float progress) {
-                mainCanvasGroup.alpha = Easing.easeInOutSine(0, 1, progress);
-            },
-            delegate {
-                controlsEnabled = true;
-            }
-        );
-    }
+	private const string LEVEL_CLEAR_SCREEN_PATH = "LevelClearScreen";
+	private static Coroutine levelClearFadeRoutine = null;
+	public static void LevelClear(float timeInSeconds)
+	{
+		Debug.Log(timeInSeconds);
+		if (instance == null)
+		{
+			GameObject gameOverScreenObject = (GameObject)Resources.Load(LEVEL_CLEAR_SCREEN_PATH);
+			GameObject instantiated = Instantiate(gameOverScreenObject);
+			DontDestroyOnLoad(instantiated);
+			instance = instantiated.GetComponent<LevelClearManager>();
+		}
+		instance.ShowLevelClearScreen(timeInSeconds);
+	}
 
-    private bool controlsEnabled = false;
-    private void Update() {
-        if (controlsEnabled) {
-            if (Input.GetKeyDown(KeyCode.Space)) {
-                LoadingScreen.LoadScene(SceneManager.GetActiveScene().name, delegate {
-                    Time.timeScale = 1;
-                    mainCanvasGroup.alpha = 0;
-                });
-                controlsEnabled = false;
-            }
-        }
-    }
+	private void ShowLevelClearScreen(float timeInSeconds)
+	{
+		gameObject.GetComponent<HighScoreManager>().setup(timeInSeconds);
+		Time.timeScale = 0;
+		this.EnsureCoroutineStopped(ref levelClearFadeRoutine);
+		this.CreateAnimationRoutine(
+				0.3f,
+				delegate (float progress)
+				{
+					mainCanvasGroup.alpha = Easing.easeInOutSine(0, 1, progress);
+				},
+				delegate
+				{
+					controlsEnabled = true;
+				}
+		);
+	}
+
+	private bool controlsEnabled = false;
+
+	public void ToggleControlsEnabled()
+	{
+		controlsEnabled = !controlsEnabled;
+	}
+
+	private void Update()
+	{
+		if (controlsEnabled)
+		{
+			if (Input.GetKeyDown(KeyCode.Space))
+			{
+				LoadingScreen.LoadScene(nextSceneName, delegate
+				{
+					Time.timeScale = 1;
+					mainCanvasGroup.alpha = 0;
+				});
+				controlsEnabled = false;
+			}
+			if (Input.GetKeyDown(KeyCode.R))
+			{
+				LoadingScreen.LoadScene(SceneManager.GetActiveScene().name, delegate
+				{
+					Time.timeScale = 1;
+					mainCanvasGroup.alpha = 0;
+				});
+				controlsEnabled = false;
+			}
+		}
+	}
 }
