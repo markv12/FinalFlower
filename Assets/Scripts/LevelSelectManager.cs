15b66cc8004337c4446f2f7cbe16afc937d404a6
Scott <swsteffes@gmail.com>
2020-04-19 18:43:49 +0900

    add level 3 to level select

--- Assets/Scripts/LevelSelectManager.cs
+++ Assets/Scripts/LevelSelectManager.cs
@@ -5,10 +5,12 @@ public class LevelSelectManager : MonoBehaviour {
 
     public Button level1Button;
     public Button level2Button;
+    public Button level3Button;
 
     void Awake() {
         level1Button.onClick.AddListener(delegate { LoadScene(Scenes.LEVEL_1); });
         level2Button.onClick.AddListener(delegate { LoadScene(Scenes.LEVEL_2); });
+        level3Button.onClick.AddListener(delegate { LoadScene(Scenes.LEVEL_3); });
     }
 
     private void LoadScene(Scenes scene) {
--- Assets/Scripts/Scenes.cs
+++ Assets/Scripts/Scenes.cs
@@ -53,6 +53,7 @@ public enum Scenes {
     LEVEL_SELECT,
     LEVEL_1,
     LEVEL_2,
+    LEVEL_3,
 }
 
 public static class ScenesExtensions {
@@ -64,8 +65,8 @@ public static class ScenesExtensions {
                 return "LevelSelect";
             case Scenes.LEVEL_1:
                 return "Level 1";
-            case Scenes.LEVEL_2:
-                return "Level 2";
+            case Scenes.LEVEL_3:
+                return "Level 3";
             default:
                 return "Scene Not Found";
         }
