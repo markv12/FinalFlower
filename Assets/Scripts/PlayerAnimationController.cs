318dd11e3cc59849fc706dbdfa09519fcef31a1b
Scott <swsteffes@gmail.com>
2020-04-20 00:24:34 +0900

    first pass at animating eyes

--- Assets/Scripts/PlayerAnimationController.cs
+++ Assets/Scripts/PlayerAnimationController.cs
@@ -5,9 +5,20 @@ public class PlayerAnimationController : MonoBehaviour {
 
     public Transform wheel;
     public Transform head;
+    public Transform pupil1;
+    private Vector3 pupil1RootPos;
+    public Transform pupil2;
+    private Vector3 pupil2RootPos;
+
+    public Transform eyeTarget;
 
     public Vector3 headLowPosition;
     public Vector3 headTopPosition;
+
+    private void Awake() {
+        pupil1RootPos = pupil1.localPosition;
+        pupil2RootPos = pupil2.localPosition;
+    }
 
     public void RotateWheel(float amount) {
         wheel.Rotate(new Vector3(0, 0, amount*-1.2f));
@@ -42,4 +53,20 @@ public class PlayerAnimationController : MonoBehaviour {
         yield return jumpSubRoutine;
     }
 
+    private void Update() {
+        pupil1.localPosition = GetClosestPointToEyeTarget(pupil1RootPos);
+        pupil2.localPosition = GetClosestPointToEyeTarget(pupil2RootPos);
+    }
+
+    private const float MAX_EYE_DISTANCE = 0.07f;
+    private Vector3 GetClosestPointToEyeTarget(Vector3 localRootPos) {
+        Vector3 worldSpacePos = head.TransformPoint(localRootPos);
+        Vector3 targetPos = eyeTarget.position;
+        Vector3 diff = targetPos - worldSpacePos;
+        if(diff.magnitude < MAX_EYE_DISTANCE) {
+            return head.InverseTransformPoint(targetPos);
+        } else {
+            return localRootPos + (diff.normalized * MAX_EYE_DISTANCE);
+        }
+    }
 }
