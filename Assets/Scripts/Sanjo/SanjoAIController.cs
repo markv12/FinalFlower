cb30b00d17947f4f5e46d1c83b39e7ee10e37283
三條明寛 <akihirosanjo@gmail.com>
2020-04-20 10:20:16 +0900

    SanjoAI

---
+++ Assets/Scripts/Sanjo/SanjoAIController.cs
@@ -0,0 +1,134 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SanjoAIController : MonoBehaviour
+{
+    public bool isWalker = false;
+	public bool isJumper = false;
+	
+    public GameObject owner;
+	public ThingToProtect target;
+
+	public float movePower = 80.0f;
+    public float jumpPowerX = 1.0f;
+    public float jumpPowerY = 8.0f;
+    public float minDistance = 0.0f;
+    public float detectRange = 12.0f;
+    public float lostRange = 18.0f;
+
+	public SanjoCollisionChecker floorChecker = null;
+	public SanjoCollisionChecker pitfallChecker = null;
+	public SanjoCollisionChecker wallChecker = null;
+	public SanjoCollisionChecker attackCollision = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = owner.GetComponent<Rigidbody2D>();
+    }
+	
+	private Rigidbody2D rigidBody;
+
+	private bool isDetected = false;
+	private bool isLanding = false;
+
+    private bool dead = false;
+
+    // Update is called once per frame
+    public void Update() 
+	{
+		Transform targetTrans;
+
+		if( !target )
+		{
+			targetTrans = Player.mainPlayer.transform;
+		}
+		else
+		{
+			targetTrans = target.transform;
+		}
+
+		if (Vector3.Distance(transform.position, target.transform.position) > 50) 
+		{
+            Destroy(gameObject);
+        }
+
+		if( dead ){ return; }
+
+		float distanceFromTarget = target.transform.position.x - transform.position.x;
+		
+		UpdateMove( distanceFromTarget );
+
+		UpdateDetection( distanceFromTarget );
+
+		UpdateContactAttack();
+		
+		wallChecker.SetOffsetX( Mathf.Sign( distanceFromTarget ) );
+		pitfallChecker.SetOffsetX( Mathf.Sign( distanceFromTarget ) );
+	}
+
+		private void UpdateMove( float distance )
+	{
+		Vector2 move = rigidBody.velocity;
+
+		if( isDetected )
+		{
+			if( floorChecker.isHit() )
+			{
+				bool obstacleAhead = wallChecker.isHit() || !pitfallChecker.isHit();
+
+				if( isWalker && !obstacleAhead )
+				{
+					float velocityX = ( movePower * Time.deltaTime * Mathf.Sign( distance ) );
+
+					if( Mathf.Abs( distance ) > minDistance )
+					{
+						move.x = velocityX;
+					}
+				}
+
+				if( isJumper )
+				{
+					if( obstacleAhead )
+					{
+						move.x = jumpPowerX;
+						move.y = jumpPowerY;
+
+						if( !pitfallChecker.isHit() )
+						{
+							move.x *= 2.0f;
+						}
+					}
+				}
+			}
+		}
+		
+		rigidBody.velocity = move;
+	}
+
+	private void UpdateDetection( float distance )
+	{
+		float absDistance = Mathf.Abs( distance );
+
+		if( isDetected )
+		{
+			if( absDistance > lostRange )
+			{
+				isDetected = false;
+			}
+		}
+		else if( absDistance < detectRange )
+		{
+			isDetected = true;
+		}
+	}
+
+	private void UpdateContactAttack()
+	{
+		if( attackCollision.isHit() )
+		{
+			Player.mainPlayer.GetHit();
+		}
+	}
+}
---
+++ Assets/Scripts/Sanjo/SanjoCollisionChecker.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SanjoCollisionChecker : MonoBehaviour
+{
+	public string tagName1;
+	public string tagName2;
+	
+	public bool isHit() { return hit; }
+
+	private int tagHash1 = 0;
+	private int tagHash2 = 0;
+
+	private BoxCollider2D box;
+
+	private Vector2 offset;
+
+	public void SetOffset( Vector2 offset )
+	{
+		box.offset = offset;
+	}
+
+	public void SetOffsetX( float offsetX )
+	{
+		box.offset = new Vector2(offsetX, box.offset.y);
+	}
+	private bool hit = false;
+	private bool enter = false;
+	private bool stay = false;
+	private bool exit = false;
+	
+	private void Start()
+	{
+		tagHash1 = tagName1.GetHashCode();
+		tagHash2 = tagName2.GetHashCode();
+
+		box = GetComponent<BoxCollider2D>();
+	}
+
+	private void FixedUpdate()
+	{
+		if( enter || stay ) 
+		{
+			hit = true; 
+		}
+		else if( exit )
+		{
+			hit = false; 
+		}
+
+		enter = false;
+		stay = false;
+		exit = false;
+	}
+
+	private bool checkHash( int hash )
+	{
+		return ( hash == tagHash1 || hash == tagHash2 );
+	}
+
+	private void OnTriggerEnter2D( Collider2D collision )
+	{
+		enter = checkHash( collision.tag.GetHashCode() );
+	}
+ 
+	private void OnTriggerStay2D( Collider2D collision )
+	{
+		stay = checkHash( collision.tag.GetHashCode() );
+	}
+     
+	private void OnTriggerExit2D( Collider2D collision )
+	{
+		exit = checkHash( collision.tag.GetHashCode() );
+	}
+}
